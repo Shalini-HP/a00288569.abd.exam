15c9bb35c94a6414d50790683976e4d472db4f04
Shalini <a00288569@student.ait.ie>
2020-12-28 17:48:25 +0000

    History feature

--- src/main/java/com/abd/exam/Service/HistoryService.java
+++ src/main/java/com/abd/exam/Service/HistoryService.java
@@ -1,6 +1,7 @@
 package com.abd.exam.Service;
 
 import com.abd.exam.common.LogType;
+import com.abd.exam.dto.LogDto;
 import com.abd.exam.model.Employee;
 import com.abd.exam.model.History;
 import com.abd.exam.repository.EmployeeRepository;
@@ -21,4 +22,20 @@ public class HistoryService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    public List<History> getHistories() {
+        return historyRepository.findAll();
+    }
+
+    public Optional<History> addHistory(LogDto logDto) {
+        History history = new History();
+        Optional<Employee> employee = employeeRepository.findByCode(logDto.getCode());
+        if(!employee.isPresent()) {
+            return Optional.empty();
+        }
+        history.setCodeUsed(logDto.getCode());
+        history.setCreatedOn(LocalDate.now());
+        history.setEmployee(employee.get());
+        history.setType(LogType.valueOf(logDto.getType()));
+        return Optional.of(historyRepository.save(history));
+    }
 }
